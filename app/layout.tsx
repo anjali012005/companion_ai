c3101a60eb53ac7858c094c84c96bb6afc531d36
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-11-25 12:21:08 +0530

    Added Mode Toggle eg. Drak theme, light theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import {
 } from '@clerk/nextjs'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,11 +33,19 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
