fe6ab18d23c8c4337b6a756edeaab83f241dbc50
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-11-26 13:36:40 +0530

    Created Sidebar+Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import {
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,7 +36,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={cn("bg-secondary",`${geistSans.variable} ${geistMono.variable} antialiased`)}
         >
           <ThemeProvider
             attribute="class"
